c155c3a07163c77c03b6dfa1a0fb229203aa8cd5
SoaresDavidson <davi504r35@gmail.com>
2025-01-14 19:17:00 -0300

    não aguento mais

--- modules/Filtros.py
+++ modules/Filtros.py
@@ -1,4 +1,4 @@
-from PIL import ImageFilter, ImageOps
+from PIL import Image, ImageFilter, ImageOps, ImageEnhance
 
 class Filtro:
     def aplicar(self, imagem):
@@ -9,12 +9,20 @@ class EscalaCinza(Filtro):
         return imagem.convert("L")
 
 class PretoBranco(Filtro):
-    def aplicar(imagem):
-        return imagem.convert("1")
+    def aplicar(self, imagem):
+        imagemcinza = imagem.convert("L")
+        limiar = 128
+        return imagemcinza.point(lambda p: 255 if p > limiar else 0)
 
 class Cartoon(Filtro):
-    def aplicar(imagem):
-        return imagem.filter(ImageFilter.FIND_EDGES)
+    def aplicar(self, imagem):
+        imagemCinza = imagem.convert("L")
+        imagemSuavizada = imagemCinza.filter(ImageFilter.SMOOTH_MORE)
+        bordas = imagem.filter(ImageFilter.FIND_EDGES)
+        imagemCartoon = Image.blend(imagemSuavizada.convert("RGB"), bordas.convert("RGB"), alpha=0.3) 
+        imagemCartoon = ImageEnhance.Contrast(imagemCartoon).enhance(1.5)  
+        imagemCartoon = ImageEnhance.Brightness(imagemCartoon).enhance(1.2) 
+        return imagemCartoon
 
 class FotoNegativa(Filtro):
     def aplicar(imagem):
