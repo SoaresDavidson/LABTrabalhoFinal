7e60df47558eb8cdb1aff11a0c461d3f189403e9
SoaresDavidson <davi504r35@gmail.com>
2025-01-14 16:20:15 -0300

    classe imagem feita

--- modules/Filtros.py
+++ modules/Filtros.py
@@ -5,30 +5,43 @@ class Filtro:
         pass
 
 class EscalaCinza(Filtro):
-    def aplicar(self, imagem):
+    def aplicar(imagem):
         return imagem.convert("L")
 
 class PretoBranco(Filtro):
-    def aplicar(self, imagem):
+    def aplicar(imagem):
         return imagem.convert("1")
 
 class Cartoon(Filtro):
-    def aplicar(self, imagem):
+    def aplicar(imagem):
         return imagem.filter(ImageFilter.FIND_EDGES)
 
 class FotoNegativa(Filtro):
-    def aplicar(self, imagem):
+    def aplicar(imagem):
         return ImageOps.invert(imagem)
 
 class Contorno(Filtro):
-    def aplicar(self, imagem):
+    def aplicar(imagem):
         return imagem.filter(ImageFilter.CONTOUR)
 
 class Blurred(Filtro):
-    def aplicar(self, imagem):
+    def aplicar(imagem):
         return imagem.filter(ImageFilter.BLUR)
 
 # Usar from PIL import Image
 # Usar Image.open("caminho") para instanciar uma imagem
 # Instanciar o filtro desejado igualando uma variável a função do filtro
 # Usar instancia do filtro.aplicar na imagem
+
+if __name__ == "__main__":
+    import PIL.Image
+    import PIL
+
+    imagem = PIL.Image.open("/home/davi/Imagens/Wallpapers/mistborn.jpeg")
+    EscalaCinza.aplicar(imagem=imagem).show()
+    PretoBranco.aplicar(imagem=imagem).show()
+    Cartoon.aplicar(imagem=imagem).show()
+    FotoNegativa.aplicar(imagem=imagem).show()
+    Contorno.aplicar(imagem=imagem).show()
+    Blurred.aplicar(imagem=imagem).show()
+
--- modules/imagem.py
+++ modules/imagem.py
@@ -1,5 +1,15 @@
-class Imagem:
-    pass
+gtifrom PIL import Image
 
-def cu():
-    print("cu")
+class imagem:
+    def __init__(self, img):
+        self.__img = Image.open(img)
+
+    def mostarImagem(self):
+        self.__img.show()
+    
+    def retornaImagem(self):
+        return self.__img
+
+if __name__ == "__main__":
+    p = imagem("/home/davi/Imagens/Wallpapers/mistborn.jpeg")
+    p.mostarImagem()
