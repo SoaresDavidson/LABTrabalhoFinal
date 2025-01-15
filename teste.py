cff4f0cee118ab9ff15768d3fc8500c59dd7f4b2
SoaresDavidson <davi504r35@gmail.com>
2025-01-15 15:05:55 -0300

    o flask consegue mandar imagem para a pagina

--- teste.py
+++ teste.py
@@ -1,7 +1,5 @@
-from modules import imagem,Filtros
+from modules.Filtros import EscalaCinza,PretoBranco,Cartoon,Contorno,FotoNegativa,Blurred
+from modules.imagem import Imagem
 
-p = imagem.imagem("/home/davi/Imagens/Wallpapers/mistborn.jpeg")
-p.mostarImagem()
-p2 = Filtros.EscalaCinza.aplicar(p.retornaImagem())
-p3 = imagem.imagem(p2)
-p3.mostarImagem()
+def aplicar_filtro
+
