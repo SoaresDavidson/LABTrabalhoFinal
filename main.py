5c8184b9f8c2e5f76a3e44c86a7214018a7de78e
SoaresDavidson <davi504r35@gmail.com>
2025-01-15 09:29:49 -0300

    placeholder para a imagem

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from modules import imagem,download,filtro
-from flask import Flask, render_template
+from modules import imagem,download,Filtros
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        imagem = "static/imagem.jpeg"
+        return render_template("imagem.html",imagem=imagem)
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- modules/download.py
+++ modules/download.py
@@ -1,2 +1,15 @@
+import requests
 class Download:
-    pass
+    def baixar(link):
+        resposta = requests.get(link)
+        if resposta.status_code == 200:
+            with open ('imagemBaixada.jpg', 'wb') as arqv:
+                arqv.write(resposta.content)
+                print ("Imagem baixada pai!")
+        else:
+            print('o download nÃ£o foi realizado.')
+        
+if __name__ == '__main__':
+   url = input("Digite o link da imagem que deseja baixar: ")
+   Download.baixar(url)
+   
