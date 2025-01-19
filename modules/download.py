8c1608db5ff0efc37aadb2f13d077870a22e6bf4
Arthur Magno <arthuredstonegamers@gmail.com>
2025-01-19 14:16:32 -0300

    alterei mais algumas coisinhas, adicionei uma nova excecao dependendo do link e fiz ela lançar uma ValueError, que é captada quando a funcao q tem esse erro eh chamada

--- modules/download.py
+++ modules/download.py
@@ -8,7 +8,8 @@ class Download:
         resposta = requests.get(link)
         if resposta.status_code == 200:
                 # Abrir a imagem diretamente a partir dos bytes recebidos
-                img = Image.open(io.BytesIO(resposta.content))  # Abre a imagem a partir do buffer de bytes
+               
+                img = Image.open(io.BytesIO(resposta.content))  # Abre a imagem a partir do buffer de bytes 
                 return img
                 # img.show()  # Exibe a imagem (opcional)
                 # img.save('imagemBaixada.jpg')  # Salva a imagem
